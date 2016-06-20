6a07548a73400e1a2a7ed1eff5ede335e6a4e9df
Ayesha <ayesha.wnz@gmail.com>
2016-06-20 12:42:47 +1200

    Original template credit

---
+++ TreeStory.java
@@ -0,0 +1,22 @@
+/**
+* Boilerplate or template originally from teamtreehouse.com.
+* Track: Java learning path/track.
+* Java Basics - Multiple strings:  to prompt for multiple variables
+* and use them in a single formatted String.
+*/
+
+import java.io.Console;
+
+public class TreeStory {
+
+    public static void main(String[] args) {
+        Console console = System.console();
+        /*  Some terms:
+            noun - Person, place or thing
+            verb - An action
+            adjective - A description used to modify or describe a noun
+            Enter your amazing code here!
+        */
+    }
+
+}
