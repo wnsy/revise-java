dd05cf0e4d8329d734dbc7bb922579167eb8d726
Ayesha <ayesha.wnz@gmail.com>
2016-06-30 18:22:07 +1200

    Initial program provided

---
+++ MiniGolf.java
@@ -0,0 +1,13 @@
+import java.io.Console;
+import java.util.Random;
+
+public class MiniGolf {
+
+
+  public static void main(String[] args) {
+    Console console = System.console();
+    Random luck = new Random();
+
+
+  }
+}
